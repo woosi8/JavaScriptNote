3b899c964c9b3be01f2e70e55b03656609b0a526
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2020-10-28 23:09:54 +0900

    Oct-28

--- quiz.js
+++ quiz.js
@@ -207,3 +207,21 @@ An arrow function expression is a compact alternative to a traditional function 
 - Not suitable for call, apply and bind methods, which generally rely on establishing a scope.
 - Can not be used as constructors.
 - Can not use yield, within its body.
+
+// Test
+// Test
+// Test
+// Test
+
+
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+function solution(s) {
+  var answer = '';
+  if(s.length % 2 === 0){
+      answer = answer.concat(s[s.length/2 -1]);
+      answer = answer.concat(s[s.length/2]);
+  } else{
+      answer = answer.concat(s[Math.floor(s.length/2)]);
+  }
+  return answer;
+}
