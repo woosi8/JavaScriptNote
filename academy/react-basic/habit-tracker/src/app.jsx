e0bb07df553c7e6515c80377d4e8051f0cf3b27c
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-01-22 23:12:09 +0900

    Add Click Event and start nav-bar

--- academy/react-basic/habit-tracker/src/app.jsx
+++ academy/react-basic/habit-tracker/src/app.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
 
-function App() {
 
-  return <Habits />
+class App extends Component {
+  state = {
+    habits: [
+        {id: 1, name:'Reading', count:0},
+        {id: 2, name:'Running', count:0},
+        {id: 3, name:'Coding', count:0},
+    ]
+  };
+  handleIncrement =(habit) =>{
+    // habit.count++;
+    // 리액트에서는 State 오브젝트를 직접 수정하는 것은 좋지않다
+    // this.setState(this.state) //더한값을 업데이트 하기 (state 오브젝트 안에 있는 값을 변경을 하면 리액트는 모르기때문에 꼭 업데이트 해줘야한다)
+    const habitse = [...this.state.habits]
+    const index = habitse.indexOf(habit);
+    habitse[index].count++;
+    // this.setState({ habits: habits}) // 아래처럼 생략가능 key:value 가 같은경우
+    this.setState({habitse})
+  }
+  handleDecrement =(habit) =>{
+    const habitMinus = [...this.state.habits]
+    const index = habitMinus.indexOf(habit);
+    const counts = habitMinus[index].count - 1;
+    habitMinus[index].count = counts < 0 ? 0 : counts;
+    this.setState({habitMinus})
+  }
+  handleDelete =(habit) =>{
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({habits})
+  }
+
+    render() {
+      return (
+      <>
+        <Navbar totalCount = {this.state.habits.filter(item => item.count >0).length} />
+        <Habits  
+                habits = {this.state.habits}
+                onIncrement = {this.handleIncrement}
+                onDecrement = {this.handleDecrement}
+                onDelete = {this.handleDelete}
+        />
+      </>
+      );
+  }
 }
-
 export default App;
--- academy/react-basic/habit-tracker/src/components/habits.jsx
+++ academy/react-basic/habit-tracker/src/components/habits.jsx
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            {id: 1, name:'Reading', count:0},
-            {id: 2, name:'Running', count:0},
-            {id: 3, name:'Coding', count:0},
-        ]
-    };
+
     handleIncrement =(habit) =>{
-        console.log(`${habit.name}`);
+        this.props.onIncrement(habit)
     }
     handleDecrement =(habit) =>{
+        this.props.onDecrement(habit)
+
     }
     handleDelete =(habit) =>{
+
+        this.props.onDelete(habit)
+
     }
     render() {
         return (
             <ul>
-                {this.state.habits.map(habit => (
-                     <Habit 
-                     key ={habit.id} 
-                     habit = {habit}
-                     onIncrement = {this.handleIncrement}
-                     onDecrement = {this.handleDecrement}
-                     onDelete = {this.handleDelete}
-                     /> 
+                {this.props.habits.map(habit => (
+<Habit 
+    key ={habit.id} 
+    habit = {habit}
+    onIncrement = {this.handleIncrement}
+    onDecrement = {this.handleDecrement}
+    onDelete = {this.handleDelete}
+    /> 
                 ))}          
             </ul>    
         );
